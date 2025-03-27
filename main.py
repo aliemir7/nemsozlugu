af36dbd28ae5c2bf8f2d22992604374e8d749246
aliemir7 <alicodincer34@gmail.com>
2025-03-27 18:39:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ meme_sozlugu = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
             "ROFL": "ROFL bir şakaya karşılıktır, LOL gibidir"
-            }
+            "UWU":UWU bir sevimli bir kelimedir             
+            "OWO":OWO uwuya benzeyen bir sevimli tatlı daha çirkin kelimedir                              
+            "ANİME":ANİME bir çinli animasyonundaki kişilere denir                                        }
 
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 
-if kelime in meme_sozlugu.keys():
-    print(meme_sozlugu[kelime])
+if kelime in nem_sozlugu.keys():
+    print(nem_sozlugu[kelime])
 else:
     print("Henüz bu kelimeye sahip değiliz... Ama üzerinde çalışıyoruz!")
